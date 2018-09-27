a3d8f6bf49487f50278690546a119783782c6a68
Stoyan <slownessbg@gmail.com>
2018-09-27 21:22:57 +0300

    Testing stuff

---
+++ hello.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Program {
+	
+	public static void Main(string[] args) {
+		int i = 44;
+		Console.WriteLine(i);
+	}
+	
+}
---
+++ loop.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Program {
+	public static void Main() {
+		
+		for (int i = 1; i <= 100;i++) {
+			if (i == 1) {
+				Console.WriteLine(5);
+			} else if (i == 2) {
+				Console.WriteLine(4);
+			}
+		}
+	}
+	
+}
